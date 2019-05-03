d9c0204440f3465c02a63747db7802e4ec48011f
sinbad <sinbad@sinbads-Mac.local>
2019-05-03 11:03:19 -0700

    ui design

--- Weather Today/Controller/Cell/DailyForcast.swift
+++ Weather Today/Controller/Cell/DailyForcast.swift
@@ -9,17 +9,66 @@
 import UIKit
 class DailyForcast : UITableViewCell {
     
+    let weatherIcon : UIImageView = UIImageView()
+    let dateNameLabel : UILabel = UILabel()
+    let weatherDescription : UILabel = UILabel()
+    let tempLabel:UILabel = UILabel()
+    let maxTemlLabel : UILabel = UILabel()
+    let maxTemImage : UIImageView = UIImageView()
+    let minTemLabel:UILabel = UILabel()
+    let minTemImage : UIImageView = UIImageView()
+    let celsiusLabel : UILabel = UILabel()
     
-    
+    let tempView : UIView = UIView()
     
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        addSubview(weatherIcon)
+        weatherIcon.translatesAutoresizingMaskIntoConstraints = false
+        weatherIcon.contentMode = .scaleAspectFit
+        weatherIcon.backgroundColor = .red
+        weatherIcon.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 10, left: 10, bottom: 0, right: 0), size: CGSize(width: 60, height: 60))
+       
+        addSubview(dateNameLabel)
+        dateNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        dateNameLabel.textColor = .black
+        dateNameLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        dateNameLabel.text = "Saturday"
+        dateNameLabel.backgroundColor = .red
+        dateNameLabel.anchor(top: topAnchor, leading: weatherIcon.trailingAnchor, bottom: nil, trailing: nil, padding: .init(top: 10, left: 5, bottom: 0, right: 0), size: CGSize(width: 250, height: 25))
         
-        // self.backgroundColor = .blue
         
         
+        addSubview(weatherDescription)
+        weatherDescription.translatesAutoresizingMaskIntoConstraints = false
+        weatherDescription.textColor = .black
+        weatherDescription.font = UIFont.systemFont(ofSize: 18)
+        weatherDescription.numberOfLines = 4
+        weatherDescription.text = "sky is clear sky is clear sky is clear sky is clear sky is clear sky is clearsky is clear sky is clear sky is clear"
+        weatherDescription.backgroundColor = .red
+        weatherDescription.anchor(top: dateNameLabel.bottomAnchor, leading: weatherIcon.trailingAnchor, bottom: nil, trailing: nil, padding: .init(top: 10, left: 5, bottom: 0, right: 0), size: CGSize(width: 250, height: 70))
+        
+        addSubview(tempView)
+        tempView.translatesAutoresizingMaskIntoConstraints = false
+        tempView.backgroundColor = .red
+        tempView.anchor(top: topAnchor, leading: weatherDescription.trailingAnchor, bottom: nil, trailing: nil, padding: .init(top: 10, left: 5, bottom: 0, right: 0), size: CGSize(width: 70, height: 90))
+        
+        tempView.addSubview(tempLabel)
+        tempLabel.translatesAutoresizingMaskIntoConstraints = false
+        tempLabel.text = "25"
+        tempLabel.textAlignment = .center
+        tempLabel.font = UIFont.boldSystemFont(ofSize: 26)
+        tempLabel.anchor(top: tempView.topAnchor, leading: tempView.leadingAnchor, bottom: nil, trailing: tempView.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0))
+        
+        
+        tempView.addSubview(celsiusLabel)
+        celsiusLabel.translatesAutoresizingMaskIntoConstraints = false
+        celsiusLabel.anchor(top: tempLabel.topAnchor, leading: nil, bottom: nil, trailing: tempLabel.trailingAnchor, padding: .init(top: 2, left: 0, bottom: 0, right: 0))
+        celsiusLabel.textColor = .black
+        celsiusLabel.text = "℃"
+        celsiusLabel.font = UIFont.boldSystemFont(ofSize: 14)
     }
     
     required init?(coder aDecoder: NSCoder) {
